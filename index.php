3893d7b887c93996c2708ba7cbb3ac9630c198b2
Técnico em Informática 2022.2 <36127512022.2@senacrjedu.local>
2022-12-14 16:21:50 -0300

    inicio no php

---
+++ index.php
@@ -0,0 +1,17 @@
+
+        <?php 
+
+        $num1 = 100;
+        $num2 = 200;
+
+        $soma = $num1 + $num2;
+
+        if ($soma >= 200){
+            echo "MAIOR QUE 200";
+        } else {
+            echo "MENOR QUE 200";
+        }
+
+        echo"<br/> O resultado da soma é {$soma}";
+
+?>
